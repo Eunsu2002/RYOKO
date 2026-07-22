ce1f5f9b9f1625f83905d4872a7940ce63e96766
mok-gi-tae <mokgitae@gmail.com>
2026-07-22 12:29:20 +0900

    maven 설정 변경 및 Place entity 추가

---
+++ demo/src/main/java/com/example/demo/entity/Place.java
@@ -0,0 +1,61 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "place")
+public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "p_name", nullable = false)
+    private String pName;
+
+    @Column(name = "body")
+    private String body;
+
+    @Column(name = "style")
+    private String style;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "p_location_lat", nullable = false)
+    private Float pLocationLat;
+
+    @Column(name = "p_location_lng", nullable = false)
+    private Float pLocationLng;
+
+    @Column(name = "avg_star")
+    private Float avgStar;
+
+    @Column(name = "review_count")
+    private Integer reviewCount;
+
+    @Column(name = "operating_hours")
+    private String operatingHours;
+
+    @Column(name = "recommended_schedule")
+    private String recommendedSchedule;
+    
+    @Column(name = "created_at", insertable = false, updatable = false )
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
+
+
