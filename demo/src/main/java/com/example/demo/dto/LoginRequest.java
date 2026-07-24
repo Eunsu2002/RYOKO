3eb509fa80d7194e5b4fe73a95a08904af1bd72d
DESKTOP-KB3HI1T\zaml5 <zaml56@naver.com>
2026-07-24 09:55:55 +0900

    오류로 인한 경로 변경

---
+++ demo/src/main/java/com/example/demo/dto/LoginRequest.java
@@ -0,0 +1,37 @@
+package com.example.demo.dto;
+
+public class LoginRequest {
+
+    private String email;
+    private String password;
+
+    public LoginRequest() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private boolean keepLoggedIn;
+
+    public boolean isKeepLoggedIn() {
+        return keepLoggedIn;
+    }
+
+    public void setKeepLoggedIn(boolean keepLoggedIn) {
+        this.keepLoggedIn = keepLoggedIn;
+    }
+    
+}
