bba8ec142e852633560cba42b6b4c573eb8812d1
mok-gi-tae <mokgitae@gmail.com>
2026-07-24 16:16:13 +0900

    feat: PlaceResponse 추가

--- demo/src/main/java/com/example/demo/place/dto/PlaceResponse.java
+++ demo/src/main/java/com/example/demo/place/dto/PlaceResponse.java
@@ -1,5 +1,34 @@
 package com.example.demo.place.dto;
 
 
-public record PlaceResponse() {
+import com.example.demo.place.Place;
+import jakarta.persistence.Column;
+
+import java.time.LocalDateTime;
+
+public record PlaceResponse(
+         int id,
+         String pName,
+         String body,
+         String style,
+         String category,
+         Float pLocationLat,
+         Float pLocationLng,
+         Float avgStar,
+         Integer reviewCount
+) {
+    public static PlaceResponse from(Place place) {
+        return new PlaceResponse(
+                place.getId(),
+                place.getPName(),
+                place.getBody(),
+                place.getStyle(),
+                place.getCategory(),
+                place.getPLocationLat(),
+                place.getPLocationLng(),
+                place.getAvgStar(),
+                place.getReviewCount()
+        );
+    }
+
 }
