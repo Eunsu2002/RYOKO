61cd86b26d636d4cc0fd5fb53c3aac63a7fedaf7
mok-gi-tae <mokgitae@gmail.com>
2026-07-24 13:16:32 +0900

    feat: PlaceRepository 생성

--- demo/src/main/java/com/example/demo/repository/PlaceRepository.java
+++ demo/src/main/java/com/example/demo/repository/PlaceRepository.java
@@ -3,6 +3,10 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PlaceRepository extends JpaRepository<Place, Integer> {
+public interface PlaceRepository
+        extends JpaRepository<Place, Integer>,
+                JpaSpecificationExecutor<Place> {
+
 }
