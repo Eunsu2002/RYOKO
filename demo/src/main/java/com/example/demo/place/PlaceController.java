70316cd341ca4fcbd5ee656a11109e0d5cae19ca
mok-gi-tae <mokgitae@gmail.com>
2026-07-24 16:39:14 +0900

    feat: PlaceController 추가

--- demo/src/main/java/com/example/demo/place/PlaceController.java
+++ demo/src/main/java/com/example/demo/place/PlaceController.java
@@ -1,9 +1,16 @@
 package com.example.demo.place;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/place")
+@RequiredArgsConstructor
 public class PlaceController {
+
+
+    // 요청받으면 여행지 리스트 페이지 죽 응답
 
 
 }
