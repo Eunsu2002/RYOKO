5e18f1dc5aa1ea8abe2ecfbe70514689e113d5e2
mok-gi-tae <mokgitae@gmail.com>
2026-07-24 17:50:54 +0900

    feat: PlaceRepository 키워드 검색 기능 추가

--- demo/src/main/java/com/example/demo/place/PlaceService.java
+++ demo/src/main/java/com/example/demo/place/PlaceService.java
@@ -15,11 +15,20 @@ public class PlaceService {
 
     private final PlaceRepository placeRepository;
 
+    // 여행지 전체 조회
     public List<PlaceResponse> getPlaces() {
         return placeRepository.findAll()
                 .stream()
                 .map(PlaceResponse::from)
                 .toList();
     }
+
+//    키워드 검색
+    public
+
+//    지역·카테고리·스타일 필터
+//    평점·리뷰 수 정렬
+//    페이징
+//    대표 사진 조회
 
 }
