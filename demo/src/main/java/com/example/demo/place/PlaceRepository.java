fcdfda92deea6607818331d3f793374ac40e4db9
mok-gi-tae <mokgitae@gmail.com>
2026-07-28 10:29:36 +0900

    feat: PlaceRepository 필터 기능 추가

--- demo/src/main/java/com/example/demo/place/PlaceRepository.java
+++ demo/src/main/java/com/example/demo/place/PlaceRepository.java
@@ -13,13 +13,22 @@ public interface PlaceRepository extends JpaRepository<Place, Integer>,
 
     // 키워드로 검색하는 기능 -- 지역 이름 및 카테고리
     @Query("""
-        SELECT p
-        FROM Place p
+        SELECT p FROM Place p
         WHERE lower(p.pName) like lower(concat('%', :keyword, '%'))
         or lower(p.address) like lower(concat('%', :keyword, '%')) 
         or lower(p.category) like lower(concat('%', :keyword, '%')) 
     """)
     List<Place> searchByKeyword (@Param("keyword") String keyword);
 
+    // 키워드로 필터 기능
+    @Query("""
+        SELECT p FROM Place p
+        WHERE (:name IS NULL OR p.pName LIKE CONCAT('%', :name, '%'))
+        AND (:style IS NULL OR p.address = :address)
+        AND (:category IS NULL OR p.category = :category)
+        """)
+    List<Place> filterByKeyword (@Param("name") String name,
+                                 @Param("address") String address,
+                                 @Param("category") String category);
 
 }
