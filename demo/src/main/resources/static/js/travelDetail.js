3e5f12fd6538307dbeba6e7f65acf4cf7519983e
mok-gi-tae <mokgitae@gmail.com>
2026-07-28 18:16:00 +0900

    feat: 일정 추가 버튼의 기능 작성

--- demo/src/main/resources/static/js/travelDetail.js
+++ demo/src/main/resources/static/js/travelDetail.js
@@ -3,9 +3,10 @@ const params = new URLSearchParams(window.location.search)
 const placeId = params.get('id')
 const $ = (selector) => document.querySelector(selector);
 
+let place = null;
+
 console.log(placeId)
 
-// id 받아서 넣기
 async function loadPlaceDetail(placeId) {
     try {
         const res = await fetch(`/api/places/${placeId}`);
@@ -27,13 +28,46 @@ function renderPlaceDetail(place) {
     $(`#detail-pName`).textContent = place.pName;
     $(`#detail-address`).textContent = place.address;
     $(`#detail-body`).textContent = place.body;
+    $(`#place-reviewCount`).textContent = `${place.reviewCount}件のレビュー`;
     $(`#detail-avgStar`).textContent = place.avgStar.toFixed(2);
     $(`#detail-operatingHours`).textContent = place.operatingHours;
     $(`#detail-recommendSchedule`).textContent = place.recommendedSchedule;
     // place 테이블에 place_img 연결 및 Place Entity 관련 추가 필요
 }
-
 // 리뷰 연결 필요
 
+// 일정에 추가 버튼 눌렀을 때 기능
+function addSchedule() {
+    if (!place) {
+        alert("旅行先の情報を読み込めませんでした。")
+        return
+    }
 
+    const startDate = prompt('訪問日を入力してください。(예: 2026-08-15)');
+    if (!startDate) return;
+
+    const visitDateTime = `${startDate}T00:00:00`;
+
+    fetch('/api/plans', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            placeId: place.id,
+            pName: place.pName,
+            pLocationLat: place.pLocationLat,
+            pLocationLng: place.pLocationLng,
+            visitDate: visitDateTime
+        })
+    })
+        .then(res =>{
+            if (!res.ok) throw new Error("日程追加失敗")
+            alert('日程に追加されました！')
+        })
+        .catch(err => {
+            console.error(err);
+            alert('日程の追加中にエラーが発生しました。')
+        });
+}
+
+$(`.schedule-btn`).addEventListener('click', addSchedule);
 loadPlaceDetail(placeId);
