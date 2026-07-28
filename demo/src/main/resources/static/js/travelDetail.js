a44cc64ea5f4c9a2373a238bca0b7f76e1fcacfb
mok-gi-tae <mokgitae@gmail.com>
2026-07-28 17:21:45 +0900

    feat: 여행지 세부 페이지 작성

--- demo/src/main/resources/static/js/travelDetail.js
+++ demo/src/main/resources/static/js/travelDetail.js
@@ -1,18 +1,39 @@
 
 const params = new URLSearchParams(window.location.search)
 const placeId = params.get('id')
+const $ = (selector) => document.querySelector(selector);
 
 console.log(placeId)
 
+// id 받아서 넣기
 async function loadPlaceDetail(placeId) {
     try {
-        const res = await fetch(`/api/places/{placeId}`)
+        const res = await fetch(`/api/places/${placeId}`);
 
         if (!res.ok) {
-            throw new Error(`서버 응답 에러: ${res.status}`)
+            throw new Error(`서버 응답 에러: ${res.status}`);
         }
 
+        const place = await res.json();
+        console.log(place);
 
+        renderPlaceDetail(place);
+    } catch (err) {
+        console.error('여행지 정보를 불러오지 못했습니다.', err);
     }
-
 }
+// place 렌더링
+function renderPlaceDetail(place) {
+    $(`#detail-pName`).textContent = place.pName;
+    $(`#detail-address`).textContent = place.address;
+    $(`#detail-body`).textContent = place.body;
+    $(`#detail-avgStar`).textContent = place.avgStar.toFixed(2);
+    $(`#detail-operatingHours`).textContent = place.operatingHours;
+    $(`#detail-recommendSchedule`).textContent = place.recommendedSchedule;
+    // place 테이블에 place_img 연결 및 Place Entity 관련 추가 필요
+}
+
+// 리뷰 연결 필요
+
+
+loadPlaceDetail(placeId);
