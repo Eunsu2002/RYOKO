a422cbb20054ae775f74403d1e13e574d7954a57
DESKTOP-KB3HI1T\zaml5 <zaml56@naver.com>
2026-09-02 11:10:18 +0900

    메인 수정 및 여행지 검색 쿼리 null 처리 수정

--- demo/src/main/java/com/example/demo/repository/PlaceRepository.java
+++ demo/src/main/java/com/example/demo/repository/PlaceRepository.java
@@ -9,40 +9,36 @@ import org.springframework.data.repository.query.Param;
 
 public interface PlaceRepository extends JpaRepository<Place, Integer> {
 
-
     @Query(value = """
-        SELECT p.* FROM place p
-        WHERE ( lower(p.p_name) LIKE LOWER(CONCAT('%', :keyword, '%'))
-        OR lower(p.address) LIKE LOWER(CONCAT('%', :keyword, '%'))
-        OR lower(p.category) LIKE LOWER(CONCAT('%', :keyword, '%')) )
-        AND (:style IS NULL OR (p.style & :style) > 0)
-    """,
-            countQuery = """
-        SELECT COUNT(*) FROM place p
-        WHERE ( lower(p.p_name) LIKE LOWER(CONCAT('%', :keyword, '%'))
-        OR lower(p.address) LIKE LOWER(CONCAT('%', :keyword, '%'))
-        OR lower(p.category) LIKE LOWER(CONCAT('%', :keyword, '%')) )
-        AND (:style IS NULL OR (p.style & :style) > 0)
-    """,
-            nativeQuery = true)
-    Page<Place> search (@Param("keyword") String keyword,
-                        @Param("style") Integer style,
-                        Pageable pageable);
+                SELECT p.* FROM place p
+                WHERE (:keyword IS NULL OR (
+                    lower(p.p_name) LIKE LOWER(CONCAT('%', :keyword, '%'))
+                    OR lower(p.address) LIKE LOWER(CONCAT('%', :keyword, '%'))
+                    OR lower(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))
+                ))
+                AND (:style IS NULL OR (p.style & :style) > 0)
+            """, countQuery = """
+                SELECT COUNT(*) FROM place p
+                WHERE (:keyword IS NULL OR (
+                    lower(p.p_name) LIKE LOWER(CONCAT('%', :keyword, '%'))
+                    OR lower(p.address) LIKE LOWER(CONCAT('%', :keyword, '%'))
+                    OR lower(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))
+                ))
+                AND (:style IS NULL OR (p.style & :style) > 0)
+            """, nativeQuery = true)
+    Page<Place> search(@Param("keyword") String keyword,
+            @Param("style") Integer style,
+            Pageable pageable);
 
-
-// 대표 사진 조회
-
-
-
-//    키워드로 필터 기능
-//    @Query("""
-//        SELECT p FROM Place p
-//        WHERE (:name IS NULL OR p.pName LIKE CONCAT('%', :name, '%'))
-//        AND (:style IS NULL OR p.address = :address)
-//        AND (:category IS NULL OR p.category = :category)
-//        """)
-//    List<Place> filterByKeyword (@Param("name") String name,
-//                                 @Param("address") String address,
-//                                 @Param("category") String category);
+    // 키워드로 필터 기능
+    // @Query("""
+    // SELECT p FROM Place p
+    // WHERE (:name IS NULL OR p.pName LIKE CONCAT('%', :name, '%'))
+    // AND (:style IS NULL OR p.address = :address)
+    // AND (:category IS NULL OR p.category = :category)
+    // """)
+    // List<Place> filterByKeyword (@Param("name") String name,
+    // @Param("address") String address,
+    // @Param("category") String category);
 
 }
